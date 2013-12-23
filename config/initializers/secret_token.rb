8fe807e5929bf0f55a965d817111071dcec50885
thebeerfridge <BeerFridge@beers-imac.wi.rr.com>
2013-12-23 12:52:33 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsfeed::Application.config.secret_key_base = 'fa0187c3ab41de7c69d84a905edcf2a44bf3691bc63bdf222be83fd0d307583517e1c2c0786bcfdc9fda51d9ed7acd7abc1efa93c8978c62ac97eeaf422b2b18'
